ea664fbbbd241f480033fc95b28da62e8fa2a0de
andreasnilsen <danduras@gmail.com>
2022-01-21 16:48:34 +0100

    do not fail on unknown properties

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -38,6 +39,7 @@ val objectMapper: ObjectMapper = ObjectMapper()
     .registerModule(JavaTimeModule())
     .registerKotlinModule()
     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
 val log: Logger = LoggerFactory.getLogger("no.nav.no.nav.syfo.pale2register")
 
