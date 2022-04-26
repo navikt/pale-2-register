2192a682032d0640d9a7df62b943ed5919985e89
Tia Firing <tia.firing@nav.no>
2022-04-26 07:41:44 +0200

    oppgraderer ktor

--- src/main/kotlin/no/nav/syfo/application/ApplicationEngine.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationEngine.kt
@@ -1,10 +1,10 @@
 package no.nav.syfo.application
 
-import io.ktor.application.ApplicationCallPipeline
-import io.ktor.routing.routing
+import io.ktor.server.application.ApplicationCallPipeline
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.routing.routing
 import no.nav.syfo.Environment
 import no.nav.syfo.application.api.registerNaisApi
 import no.nav.syfo.metrics.monitorHttpRequests
