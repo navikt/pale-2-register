1392a35006bf45318bec44ca43f9338c93eab36e
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-12-04 08:01:44 +0100

    Added in org.flywaydb:flyway-database-postgresql

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,8 @@ dependencies {
 
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    compileOnly("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
 
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
