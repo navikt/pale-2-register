bccb485fb3cd0bac4e53a2a2cd09800f73097a38
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-08-14 09:29:50 +0200

    chore: override transient dependencies due to vulnerabilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,30 @@
 group = "no.nav.syfo"
 version = "1.0.0"
 
-val ktorVersion="2.3.12"
-val logbackVersion="1.5.6"
-val logstashencoderVersion="8.0"
-val prometheusVersion="0.16.0"
-val junitjupiterVersion="5.10.3"
-val pale2commonVersion="2.0.9"
-val jacksonVersion="2.17.2"
-val postgresVersion="42.7.3"
-val flywayVersion="10.17.1"
-val hikariVersion="5.1.0"
-val testcontainerVersion="1.20.1"
-val mockkVersion="1.13.12"
-val kotlinVersion="2.0.10"
-val googlecloudstorageVersion="2.41.0"
-val ktfmtVersion="0.44"
-val jvmVersion= "17"
+val ktorVersion = "2.3.12"
+val logbackVersion = "1.5.6"
+val logstashencoderVersion = "8.0"
+val prometheusVersion = "0.16.0"
+val junitjupiterVersion = "5.10.3"
+val pale2commonVersion = "2.0.9"
+val jacksonVersion = "2.17.2"
+val postgresVersion = "42.7.3"
+val flywayVersion = "10.17.1"
+val hikariVersion = "5.1.0"
+val testcontainerVersion = "1.20.1"
+val mockkVersion = "1.13.12"
+val kotlinVersion = "2.0.10"
+val googlecloudstorageVersion = "2.41.0"
+val ktfmtVersion = "0.44"
+val jvmVersion = "17"
 val snappyJavaVersion = "1.1.10.6"
+val commonsCompressVersion = "1.27.0"
+val nettyCodecHttp = "4.1.112.Final"
 
 plugins {
     id("application")
     kotlin("jvm") version "2.0.10"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.0"
     id("com.diffplug.spotless") version "6.25.0"
 }
 
@@ -44,6 +46,11 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    constraints {
+        implementation("io.netty:netty-codec-http:$nettyCodecHttp") {
+            because("override transient from io.ktor:ktor-server-netty")
+        }
+    }
 
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
@@ -75,6 +82,11 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation("org.testcontainers:postgresql:$testcontainerVersion")
+    constraints {
+        implementation("org.apache.commons:commons-compress:$commonsCompressVersion") {
+            because("Due to vulnerabilities, see CVE-2024-26308")
+        }
+    }
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
@@ -83,9 +95,9 @@ dependencies {
 
 tasks {
     shadowJar {
-mergeServiceFiles {
-     setPath("META-INF/services/org.flywaydb.core.extensibility.Plugin")
- }
+        mergeServiceFiles {
+            setPath("META-INF/services/org.flywaydb.core.extensibility.Plugin")
+        }
         archiveBaseName.set("app")
         archiveClassifier.set("")
         isZip64 = true
