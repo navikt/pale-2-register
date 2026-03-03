ad02eb6780e70ff37c1348ce3065f7e9ab6dd15a
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2026-03-03 13:12:43 +0100

    Refactor TestDB to use updated PostgreSQLContainer

--- src/test/kotlin/no/nav/syfo/db/TestDB.kt
+++ src/test/kotlin/no/nav/syfo/db/TestDB.kt
@@ -4,31 +4,25 @@ import io.mockk.every
 import io.mockk.mockk
 import java.sql.Connection
 import no.nav.syfo.EnvironmentVariables
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 
 class TestDB private constructor() {
     companion object {
         var database: DatabaseInterface
+        var postgres =
+            PostgreSQLContainer(DockerImageName.parse("postgres:14"))
+                .withPassword("password")
+                .withUsername("postgres")
+                .withDatabaseName("postgres")
 
         init {
-            val postgres =
-                PostgreSQLContainer<Nothing>("postgres:14").apply {
-                    withCommand("postgres", "-c", "wal_level=logical")
-                    withUsername("username")
-                    withPassword("password")
-                    withDatabaseName("database")
-                    withInitScript("db/testdb-init.sql")
-                    start()
-                    println(
-                        "Database: jdbc:postgresql://localhost:$firstMappedPort/test startet opp, credentials: test og test"
-                    )
-                }
-
+            postgres.start()
             val mockEnv = mockk<EnvironmentVariables>(relaxed = true)
-            every { mockEnv.databaseUsername } returns postgres.username
-            every { mockEnv.databasePassword } returns postgres.password
-            every { mockEnv.dbName } returns postgres.databaseName
             every { mockEnv.dbPort } returns postgres.firstMappedPort.toString()
+            every { mockEnv.databaseUsername } returns "postgres"
+            every { mockEnv.databasePassword } returns "password"
+            every { mockEnv.dbName } returns "postgres"
             database = Database(mockEnv)
         }
     }
